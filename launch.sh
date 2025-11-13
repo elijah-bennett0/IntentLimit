a56e9f5807a757ee8e814b76a6d89877dbe6df1c
Ginsu <elijahb451@gmail.com>
2025-11-13 08:26:39 -0700

    test

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@ VENV_DIR="venv"
 if [ ! -d "$VENV_DIR" ]; then
 
 	echo "[*] Installing system packages..."
-	sudo apt install python3.11-venv
+	sudo apt update && apt install python3.11-venv
 
 	echo "[*] Creating virtual environment..."
 	python3 -m venv "$VENV_DIR"

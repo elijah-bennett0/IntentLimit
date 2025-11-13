d99f1348ea06ade2785a7aff7f5ea461a797d625
root <root@pc.>
2025-11-13 07:47:07 -0700

    feat: adding launch script for easier install and portability

---
+++ launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+VENV_DIR="venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+
+	echo "[*] Creating virtual environment..."
+	python3 -m venv "$VENV_DIR"
+	source "$VENV_DIR/bin/activate"
+
+	echo "[*] Installing dependencies..."
+	pip install --upgrade pip
+	pip install -r requirements.txt
+else
+	source "$VENV_DIR/bin/activate"
+fi
+
+exec python3 IntentLimit.py "$@"

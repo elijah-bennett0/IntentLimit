1bcedfc08b6d0e4c6b6de556cba2466692682374
Ginsu <elijahb451@gmail.com>
2025-11-18 07:04:10 -0700

    feat: angr_fuzz basic angr flag solver

--- plugins/angr_fuzz/angr_fuzz.py
+++ plugins/angr_fuzz/angr_fuzz.py
@@ -2,4 +2,27 @@ __all__ = ["angr_fuzz"]
 
 def angr_fuzz(handler, params):
 
-	pass
+	elf = params["binpath"]
+	base_addr  = int(params["base"], 16)
+	solve_addr = int(params["solve"], 16)
+	avoid_addr = int(params["avoid"], 16)
+	print(type(params['flagsize']))
+	FLAG_SIZE  = params["flagsize"] if params["flagsize"] is not 'None' else 32
+
+	sym_chars = [claripy.BVS('flag_%d'%i, 8) for i in range(int(FLAG_SIZE))]
+	sym_flag  = claripy.Concat(*sym_chars + [claripy.BVV(b'\n')])
+
+	proj = angr.Project(f"./{elf}", main_opts={'base_addr':base_addr})
+	state = proj.factory.full_init_state(args=[f'./{elf}'], stdin=sym_flag)
+
+	for c in sym_chars:
+		# keep chars in readable range
+		state.solver.add(c >= ord("!"), c <= ord("~"))
+
+	# simulation manager
+	simgr = proj.factory.simulation_manager(state)
+	simgr.explore(find=solve_addr, avoid=avoid_addr)
+
+	if (len(simgr.found) > 0):
+        	for found in simgr.found:
+                	print(found.posix.dumps(0))

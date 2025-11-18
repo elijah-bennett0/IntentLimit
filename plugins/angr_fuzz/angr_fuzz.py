5667d2118f2ae80ce0569d421a7875a6808f6f6d
Ginsu <elijahb451@gmail.com>
2025-11-18 07:27:35 -0700

    fix: angr fix

--- plugins/angr_fuzz/angr_fuzz.py
+++ plugins/angr_fuzz/angr_fuzz.py
@@ -1,3 +1,6 @@
+import angr
+import claripy
+
 __all__ = ["angr_fuzz"]
 
 def angr_fuzz(handler, params):
@@ -6,13 +9,14 @@ def angr_fuzz(handler, params):
 	base_addr  = int(params["base"], 16)
 	solve_addr = int(params["solve"], 16)
 	avoid_addr = int(params["avoid"], 16)
-	print(type(params['flagsize']))
 	FLAG_SIZE  = params["flagsize"] if params["flagsize"] is not 'None' else 32
+
+	handler.Print('i', "Fuzzing binary...\n")
 
 	sym_chars = [claripy.BVS('flag_%d'%i, 8) for i in range(int(FLAG_SIZE))]
 	sym_flag  = claripy.Concat(*sym_chars + [claripy.BVV(b'\n')])
 
-	proj = angr.Project(f"./{elf}", main_opts={'base_addr':base_addr})
+	proj = angr.Project(f"{elf}", main_opts={'base_addr':base_addr})
 	state = proj.factory.full_init_state(args=[f'./{elf}'], stdin=sym_flag)
 
 	for c in sym_chars:
@@ -25,4 +29,4 @@ def angr_fuzz(handler, params):
 
 	if (len(simgr.found) > 0):
         	for found in simgr.found:
-                	print(found.posix.dumps(0))
+                	handler.Print('s', f"Output: {found.posix.dumps(0)}\n")
